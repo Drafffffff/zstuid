dfe55440e83d0e16c761c654deaa04964107c407
drafffffff <i0.0ia@qq.com>
2022-04-27 14:48:36 +0800

    change

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    sassOptions: {
-        includePaths: [path.join(__dirname, "styles")],
-    },
-    images: {
-        domains: ["127.0.0.1", "localhost", "192.168.1.4"],
-    },
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        // ignoreBuildErrors: true,
-    },
-    swcMinify: false
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  images: {
+    domains: ["127.0.0.1", "localhost", "192.168.1.4", "10.12.3.72"],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    // ignoreBuildErrors: true,
+  },
+  swcMinify: false,
 };
 
 const withTM = require("next-transpile-modules")(["gsap"]);
